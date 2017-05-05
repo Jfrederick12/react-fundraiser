294a6862f2f9491a1d467da03709332d56d805cb
Justin Frederick <jfrederickdesign@gmail.com>
2017-05-05 16:03:25 -0500

    adjust styling

---
+++ src/components/fundraiser-card.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import ProgressBar from './progress-bar';
+import '../styles/fundraiser-card.css';
+
+class FundraiserCard extends Component {
+	constructor(props) {
+		super(props);
+
+		// this.state = {
+		// 	amountRaised: 0,
+		// }
+	}
+
+	render() {
+		// let amount = Math.round((this.state.amountRaised / this.props.fund.goal) * 100)
+		return(
+			<div>
+			  <p>{this.props.fund.goal}</p>
+
+			</div>
+		)
+	}
+}
+
+export default FundraiserCard;
--- src/components/fundraiser.js
+++ src/components/fundraiser.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Data from '../data/fundraisers.json';
 import Fund from './fund';
+import FundraiserCard from './fundraiser-card';
 import '../styles/fundraiser.css';
 
 class Fundraiser extends Component {
@@ -11,18 +12,31 @@ class Fundraiser extends Component {
     }
   }
 
-
-
+  onPledgeSubmit(amount) {
+  	this.setState({
+  		amountRaised: (this.state.amountRaised + amount)
+  	})
+  }  
 
   render() {
     return (
       <div className="App">
-        {this.state.fundraisers.map((fund) => {
-        	return < Fund fund={fund} />
-        })}
+      	<div className="flex-grid">
+	        {this.state.fundraisers.map((fund) => {
+	        	return (
+			        <div className="col" key={fund.id}>
+		        		< Fund fund={fund} />
+		        	</div>
+	        	)
+	        })}
+        </div>
       </div>
     );
   }
 }
 
 export default Fundraiser;
+
+        		// <div key={fund.id}>
+	        	// 	< Fund fund={fund} />
+	        	// </div>
