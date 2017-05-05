ac18d3f21609427d9ba81db912b9bf17fa04c8e2
Justin Frederick <jfrederickdesign@gmail.com>
2017-05-05 13:40:06 -0500

    update component structure

---
+++ src/components/fund.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import DonateButton from './donate-button';
+import ProgressBar from './progress-bar';
+import '../styles/fundraiser.css';
+
+class Fund extends Component {
+	constructor(props) {
+		super(props);
+
+    this.state = {
+      amountRaised: 0,
+      agree: true
+    }		
+    this.onPledgeSubmit = this.onPledgeSubmit.bind(this);
+	}
+
+  onPledgeSubmit(amount) {
+  	this.setState({
+  		amountRaised: (this.state.amountRaised + amount)
+  	})
+  }
+
+  render() {
+		let amount = Math.round((this.state.amountRaised / this.props.fund.goal) * 100)
+		return(
+      <div className="fund-container" key={this.props.fund.id}>
+  	    <div className="bubble">
+		      <span>
+		      	{ amount < 100 ?
+						    `$${this.props.fund.goal - this.state.amountRaised} still needed for the fund`
+					    :
+					      `Congrats! the fund has raised $${this.state.amountRaised}` 
+		      	}
+			    </span>
+		    </div>
+        <div>< ProgressBar amount={amount}/></div>
+        <h1>{this.props.fund.name}</h1>
+        < DonateButton onPledgeSubmit={this.onPledgeSubmit} fund={this.props.fund} />
+        <span><em>${this.state.amountRaised}</em> of ${this.props.fund.goal} raised.</span>
+        <span>{`${this.props.fund.doners.length} backers`}</span>
+        <p className="description">{this.props.fund.description}</p>
+      </div>
+		)          	
+  }
+}
+
+export default Fund;
--- src/components/fundraiser.js
+++ src/components/fundraiser.js
@@ -1,54 +1,24 @@
 import React, {Component} from 'react';
 import Data from '../data/fundraisers.json';
-import DonateButton from './donate-button';
-import ProgressBar from './progress-bar';
+import Fund from './fund';
 import '../styles/fundraiser.css';
 
 class Fundraiser extends Component {
   constructor() {
     super();
     this.state = {
-      fundraisers: Data,
-      amountRaised: 0,
-      agree: true
+      fundraisers: Data
     }
-    this.onPledgeSubmit = this.onPledgeSubmit.bind(this);
   }
 
-  onPledgeSubmit(amount) {
-  	this.setState({
-  		amountRaised: (this.state.amountRaised + amount)
-  	})
-  }
+
 
 
   render() {
     return (
       <div className="App">
         {this.state.fundraisers.map((fund) => {
-          {
-				  	let amount = Math.round((this.state.amountRaised / fund.goal) * 100)
-          		return(
-		            <div className="fund-container" key={fund.id}>
-            	    <div className="bubble">
-							      <span>
-							      	{ amount < 100 ?
-											    `$${fund.goal - this.state.amountRaised} still needed for the fund`
-										    :
-										      `Congrats! the fund has raised $${this.state.amountRaised}` 
-							      	}
-								    </span>
-							    </div>
-		              <div>< ProgressBar amount={amount}/></div>
-		              <h1>{fund.name}</h1>
-					        < DonateButton onPledgeSubmit={this.onPledgeSubmit} fund={fund} />
-		              <span><em>${this.state.amountRaised}</em> of ${fund.goal} raised.</span>
-		              <span>{`${fund.doners.length} backers`}</span>
-					        <p className="description">{fund.description}</p>
-		            </div>
-          		)
-	          
-	        } 
+        	return < Fund fund={fund} />
         })}
       </div>
     );
